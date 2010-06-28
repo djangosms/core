532be4f3f81d0f23dc7a8550970ae4483e3474c1
Malthe Borch <mborch@gmail.com>
2010-06-28 10:20:28 +0300

    Encode to UTF-8 before outputting.

--- src/djangosms/core/logging.py
+++ src/djangosms/core/logging.py
@@ -6,12 +6,12 @@ def format_incoming(message):
     for i, request in enumerate(requests):
         time = message.time and message.time.isoformat() or '-'
         print >> out, "%d/%d %s" % (i+1, len(requests), time)
-        print >> out, "--> %s" % (request.text or "(empty)")
+        print >> out, "--> %s" % (request.text.encode('utf-8') or "(empty)")
         print >> out, "----" + "-"*len(request.text)
 
         responses = request.responses.all()
         for j, response in enumerate(responses):
             print >> out, "    %d/%d %s" % (j+1, len(responses), response.uri)
-            print >> out, "    <-- %s" % (response.text or "(empty)")
+            print >> out, "    <-- %s" % (response.text.encode('utf-8') or "(empty)")
 
     return out.getvalue()
